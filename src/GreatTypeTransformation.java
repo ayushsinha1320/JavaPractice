970d834edf9563b2160d8d2f2d546850c97dd2a5
Ayush Sinha <ayushs@testvagrant.com>
2024-09-30 17:20:55 +0530

    GreatTypeTransformation completed

---
+++ src/GreatTypeTransformation.java
@@ -0,0 +1,9 @@
+public class GreatTypeTransformation {
+    public static void main(String[] args) {
+        double weight = 79.60;
+
+        int newWeight = (int) weight;
+        System.out.println(newWeight);
+    }
+
+}
