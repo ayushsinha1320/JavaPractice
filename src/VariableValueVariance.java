fe4c4b5a108922c21720f8f47298806667f52591
Ayush Sinha <ayushs@testvagrant.com>
2024-09-30 17:09:27 +0530

    VariableValueVariance completed

---
+++ src/VariableValueVariance.java
@@ -0,0 +1,10 @@
+public class VariableValueVariance {
+    public static void main(String[] args) {
+        int value = 13;
+        System.out.println(value);
+
+        value = 79;
+        System.out.println(value);
+    }
+
+}
