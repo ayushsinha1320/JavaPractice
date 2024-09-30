ae1253a56cfabd66278abbc3f883a6b136e23cec
Ayush Sinha <ayushs@testvagrant.com>
2024-09-30 16:53:16 +0530

    RightTypeRiddle completed

---
+++ src/RightTypeRiddle.java
@@ -0,0 +1,13 @@
+public class RightTypeRiddle {
+    public static void main(String[] args){
+        int number = 79;
+        double weight = 13.20;
+        char blood = 'A';
+        boolean isEmployee = true;
+
+        System.out.println(number + " is an integer");
+        System.out.println(weight + " is a double");
+        System.out.println(blood + " is a char");
+        System.out.println(isEmployee + " is a string");
+    }
+}
