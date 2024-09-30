f5cf15b9956776ead16b166341dead4be51a21a1
Ayush Sinha <ayushs@testvagrant.com>
2024-09-30 17:05:33 +0530

    VariableVoyage completed

---
+++ src/VariableVoyage.java
@@ -0,0 +1,9 @@
+public class VariableVoyage {
+    public static void main(String[] args) {
+        int first = 13;
+        int second  = 7;
+
+        int sum = first + second;
+        System.out.println(sum);
+    }
+}
